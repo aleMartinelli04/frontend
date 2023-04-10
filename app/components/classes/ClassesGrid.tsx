fc25d8f46fd9f0369831692db8ebba21f86071dd
alessandro <martinellialessandro04@gmail.com>
2023-04-10 18:52:54 +0200

    ux improvements

--- app/components/classes/ClassesGrid.tsx
+++ app/components/classes/ClassesGrid.tsx
@@ -2,6 +2,7 @@ import {Grid, Text} from "@mantine/core";
 import {ClassCard} from "~/components/classes/ClassCard";
 import type {Class} from "~/types/types";
 
+
 export function ClassesGrid({classes}: { classes: Class[] }) {
     if (classes.length === 0) {
         return (
@@ -12,7 +13,7 @@ export function ClassesGrid({classes}: { classes: Class[] }) {
     return (
         <Grid>
             {classes.map((c) => (
-                <Grid.Col span={3} key={c.id}>
+                <Grid.Col xs={4} md={3} key={c.id}>
                     <ClassCard c={c}/>
                 </Grid.Col>
             ))}
