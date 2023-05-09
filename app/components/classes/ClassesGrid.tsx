65089217f0654f4610999e66811e90b188c2fde4
alessandro <martinellialessandro04@gmail.com>
2023-05-09 11:53:55 +0200

    🦦

--- app/components/classes/ClassesGrid.tsx
+++ app/components/classes/ClassesGrid.tsx
@@ -1,22 +1,44 @@
-import {Grid, Text} from "@mantine/core";
+import {Grid, Text, TextInput} from "@mantine/core";
 import {ClassCard} from "~/components/classes/ClassCard";
 import type {Class} from "~/types/types";
+import type {ChangeEvent} from "react";
+import {useState} from "react";
+import {IconSearch} from "@tabler/icons-react";
 
 
 export function ClassesGrid({classes}: { classes: Class[] }) {
+    const [query, setQuery] = useState<string>('');
+    const [filteredClasses, setFilteredClasses] = useState<Class[]>(classes);
+
     if (classes.length === 0) {
         return (
             <Text>Non ci sono classi!</Text>
         );
     }
 
+    const handleSearchChange = (event: ChangeEvent<HTMLInputElement>) => {
+        const {value} = event.currentTarget;
+        setQuery(value);
+        setFilteredClasses(classes.filter((c) => c.name.toLowerCase().includes(value.toLowerCase())));
+    };
+
     return (
-        <Grid>
-            {classes.map((c) => (
-                <Grid.Col xs={3} md={2} key={c.id}>
-                    <ClassCard c={c}/>
-                </Grid.Col>
-            ))}
-        </Grid>
+        <>
+            <TextInput
+                placeholder={"Ricerca classe"}
+                mb={"md"}
+                icon={<IconSearch size="0.9rem" stroke={1.5}/>}
+                value={query}
+                onChange={handleSearchChange}
+            />
+
+            <Grid>
+                {filteredClasses.map((c) => (
+                    <Grid.Col xs={3} md={2} key={c.id}>
+                        <ClassCard c={c}/>
+                    </Grid.Col>
+                ))}
+            </Grid>
+        </>
     );
 }
--- app/components/courses/CoursesGrid.tsx
+++ app/components/courses/CoursesGrid.tsx
@@ -1,22 +1,44 @@
 import type {Course} from "~/types/types";
-import {Grid, Text} from "@mantine/core";
+import {Grid, Text, TextInput} from "@mantine/core";
 import {CourseCard} from "~/components/courses/CourseCard";
+import type {ChangeEvent} from "react";
+import {useState} from "react";
+import {IconSearch} from "@tabler/icons-react";
 
 
 export function CoursesGrid({courses}: { courses: Course[] }) {
+    const [query, setQuery] = useState<string>('');
+    const [filteredCourses, setFilteredCourses] = useState<Course[]>(courses);
+
     if (courses.length === 0) {
         return (
             <Text>Non ci sono corsi!</Text>
         );
     }
 
+    const handleSearchChange = (event: ChangeEvent<HTMLInputElement>) => {
+        const {value} = event.currentTarget;
+        setQuery(value);
+        setFilteredCourses(courses.filter((c) => c.name.toLowerCase().includes(value.toLowerCase())));
+    }
+
     return (
-        <Grid>
-            {courses.map((course) => (
-                <Grid.Col xs={4} md={3} key={course.id}>
-                    <CourseCard course={course}/>
-                </Grid.Col>
-            ))}
-        </Grid>
+        <>
+            <TextInput
+                placeholder={"Ricerca corso"}
+                mb={"md"}
+                icon={<IconSearch size="0.9rem" stroke={1.5}/>}
+                value={query}
+                onChange={handleSearchChange}
+            />
+
+            <Grid>
+                {filteredCourses.map((course) => (
+                    <Grid.Col xs={4} md={3} key={course.id}>
+                        <CourseCard course={course}/>
+                    </Grid.Col>
+                ))}
+            </Grid>
+        </>
     )
 }
